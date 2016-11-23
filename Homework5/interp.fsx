710f8bca67965767ce09ab8fe86969bad054afeb
Palacios95 <apala067@fiu.edu>
2016-11-22 21:10:34 -0500

    Changed format

--- Homework5/interp.fsx
+++ Homework5/interp.fsx
@@ -23,37 +23,30 @@ let rec subst e x t= match e with
 // are propagated, how rule (6) is implemented, and how stuck evaluations
 // are reported using F#'s sprintf function to create good error messages.
 let rec interp = function
-| NUM n -> NUM n
-| BOOL e -> BOOL e
-| ID e -> ID e
-| SUCC -> SUCC
-| PRED -> PRED 
-| ISZERO -> ISZERO
-| ERROR s -> ERROR s
-| FUN (x,e) -> FUN (x,e)
-| REC (x,e) -> subst e x e
-| APP (e1, e2) ->
-    match (interp e1, interp e2) with
-    | (ERROR s, _)      -> ERROR s        // ERRORs are propagated
-    | (_, ERROR s)      -> ERROR s
-    | (SUCC, NUM n)     -> NUM (n+1)      // Rule (6)
-    | (SUCC, v)         -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
-    | (PRED, NUM 0)     -> NUM 0
-    | (PRED, NUM n)     -> NUM (n - 1)
-    | (PRED, v)         -> ERROR (sprintf "'pred' needs int argument, not '%A'" v)
-    | (ISZERO, NUM 0)   -> BOOL true
-    | (ISZERO, NUM n)   -> BOOL false
-    | (ISZERO, v)       -> ERROR (sprintf "'iszero' needs int argument, not '%A'" v)
-    | (FUN (x,e), t)    -> interp ( subst e x t)
-    | f -> snd f
-| IF (e1, e2, e3) -> 
-    match (interp e1, interp e2, interp e3) with
-    | (ERROR s, _, _)       -> ERROR s        // ERRORs are propagated
-    | (_, ERROR s, _)       -> ERROR s
-    | (_, _, ERROR s)       -> ERROR s
-    | (BOOL true, e, _)  ->  e
-    | (BOOL false, _, e) ->  e
-    | (v, _, _)             -> ERROR (sprintf "'if' needs a boolean argument for the 'if' branch, not '%A'" v)
+    | APP (e1, e2) ->
+        match (interp e1, interp e2) with
+        | (ERROR s, _)          -> ERROR s               // ERRORs are propagated
+        | (_, ERROR s)          -> ERROR s
+        | (SUCC, NUM n)         -> NUM (n+1)             // Rule (6)
+        | (SUCC, v)             -> ERROR (sprintf "'SUCC' needs INT argument, not '%A'" v)
+        | (PRED, NUM 0)         -> NUM 0
+        | (PRED, NUM n)         -> NUM (n - 1)
+        | (PRED, v)             -> ERROR (sprintf "'PRED' needs INT argument, not '%A'" v)
+        | (ISZERO, NUM 0)       -> BOOL true
+        | (ISZERO, NUM n)       -> BOOL false
+        | (ISZERO, v)           -> ERROR (sprintf "'ISZERO' needs INT argument, not '%A'" v)
+        | (FUN (x, e), v1)      -> interp (subst e x v1) // Rule (10)
+        | (REC (x, f), e)       -> interp (APP (subst f x (REC (x, f)), e)) // Rule (11)
+        | (s1, s2)              -> APP (s1, s2)
+     | IF (b, e1, e2) ->     // Rule (4 & 5)
+        match (interp b, e1, e2) with
+        | (ERROR s, _, _)       -> ERROR s
+        | (_, ERROR s, _)       -> ERROR s
+        | (_, _, ERROR s)       -> ERROR s
+        | (BOOL true, e, _)     -> interp e
+        | (BOOL false, _, e)    -> interp e
+        | (b, _, _) -> ERROR "'IF' needs BOOL expression."
+    | e -> e //Catch all case
 
   
     
