abf2c64f1c86f6a3ace82d9d93e333f96ef27a93
Palacios95 <apala067@fiu.edu>
2016-11-22 21:05:03 -0500

    Done

--- Homework5/interp.fsx
+++ Homework5/interp.fsx
@@ -9,9 +9,15 @@ System.Environment.set_CurrentDirectory __SOURCE_DIRECTORY__;;
 // and to constructors like "Parser.Parse.APP" simply as "APP".
 open Parser.Parse
 
-let rec subst app v n = match app with
-                       | APP (term, ID v) -> APP (term,  n)
-                       | APP (term, app2) -> APP (term,  (subst app2 v n) )
+//e is the body, x is the term to subsitute, t is the subsitution term
+let rec subst e x t= match e with
+                     |ID y              -> if x = y then t else ID y
+                     |APP (e1, e2)      -> APP ( subst e1 x t, subst e2 x t)
+                     |IF (e1, e2, e3)   -> IF (subst e1 x t, subst e2 x t, subst e3 x t)
+                     |FUN (y, e1)       -> if x = y then FUN (y, e1) else FUN (y, subst e1 x t)
+                     |REC (y, e1)       -> if x = y then REC (y, e1) else  REC (y, subst e1 x t)
+                     |e                 -> e
+                  
 
 // Here I show you a little bit of the implementation of interp. Note how ERRORs
 // are propagated, how rule (6) is implemented, and how stuck evaluations
@@ -25,6 +31,7 @@ let rec interp = function
 | ISZERO -> ISZERO
 | ERROR s -> ERROR s
 | FUN (x,e) -> FUN (x,e)
+| REC (x,e) -> subst e x e
 | APP (e1, e2) ->
     match (interp e1, interp e2) with
     | (ERROR s, _)      -> ERROR s        // ERRORs are propagated
@@ -37,17 +44,16 @@ let rec interp = function
     | (ISZERO, NUM 0)   -> BOOL true
     | (ISZERO, NUM n)   -> BOOL false
     | (ISZERO, v)       -> ERROR (sprintf "'iszero' needs int argument, not '%A'" v)
-    | (FUN (v,app), n)  -> interp (subst app v n) 
+    | (FUN (x,e), t)    -> interp ( subst e x t)
+    | f -> snd f
 | IF (e1, e2, e3) -> 
     match (interp e1, interp e2, interp e3) with
     | (ERROR s, _, _)       -> ERROR s        // ERRORs are propagated
     | (_, ERROR s, _)       -> ERROR s
     | (_, _, ERROR s)       -> ERROR s
-    | (BOOL true, ID e, _)  -> ID e
-    | (BOOL false, _, ID e) -> ID e
-    | (BOOL true, v, _)     -> ERROR (sprintf "'if' needs a variable argument for the 'then' branch, not '%A'" v)
-    | (BOOL false, _, v)    -> ERROR (sprintf "'if' needs a variable argument for the 'else' branch, not '%A'" v)
-    | (v, _, _)             -> ERROR (sprintf "'if' needs a boolean argument for the if branch, not '%A'" v)
+    | (BOOL true, e, _)  ->  e
+    | (BOOL false, _, e) ->  e
+    | (v, _, _)             -> ERROR (sprintf "'if' needs a boolean argument for the 'if' branch, not '%A'" v)
 
   
     
